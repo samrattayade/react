1a3ce99f9ecfd36ce3a069f5c22d3cf359dee409
samrat tayade <36564519+samrattayade@users.noreply.github.com>
2024-08-17 18:45:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Hello from './Hello';
+import Footer from './Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hello />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
